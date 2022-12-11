7b8853edb9a9ba3510224c8c55468968d52ca26c
AseelFani <120315625+AseelFani@users.noreply.github.com>
2022-12-11 16:00:01 +0200

    Add files via upload

---
+++ Constants/YossiRestaurants.ts
@@ -0,0 +1,24 @@
+import { Restaurant, YossiRestaurant } from '../Components/Intefaces/IRestaurant';
+import Mashya from '../assets/Cards/yossiRes1.png';
+import Kitchen from '../assets/Cards/yossiRes2.png';
+import Onza from '../assets/Cards/yossiRes3.png';
+
+
+
+export const YossiRestaurants:YossiRestaurant[]=[
+    {
+        name:'Onza',
+        imag:Onza,
+
+    },
+    {
+        name:'Kitchen Market',
+        imag:Kitchen
+    },
+    {
+        name:'Mashya',
+        imag:Mashya
+    },
+      
+    
+]
